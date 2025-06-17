197e4a949b2cf2785d95dd20e7e5d2d527059977
Sam <samweldon9393@gmail.com>
2025-06-17 15:02:03 -0400

    Add captions with the years

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import "./style.css";
 
-const images = [
-  "./images/1886.jpg",
-  "./images/1891.jpg",
-  "./images/1897.jpg",
-  "./images/1903.jpg",
-  "./images/1911.jpg",
-  "./images/1924.jpg",
+const maps = [
+    { src: "./images/1886.jpg", year: "1886" },
+    { src: "./images/1891.jpg", year: "1891" },
+    { src: "./images/1897.jpg", year: "1897" },
+    { src: "./images/1903.jpg", year: "1903" },
+    { src: "./images/1911.jpg", year: "1911" },
+    { src: "./images/1924.jpg", year: "1924" }
 ];
 
 function App() {
@@ -15,21 +15,24 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % images.length);
+      setIndex((prev) => (prev + 1) % maps.length);
     }, 4000); // Change slide every 3 seconds
     return () => clearInterval(interval);
   }, []);
 
   return (
       <div className="slideshow-container">
-      {images.map((src, i) => (
+      {maps.map((map, i) => (
           <img
-          key={i}
-          src={src}
-          alt={`Map ${i}`}
-          className={`slideshow-image ${i === index ? 'visible' : ''}`}
+              key={i}
+              src={map.src}
+              alt={`Map from ${map.year}`}
+              className={`slideshow-image ${i === index ? 'visible' : ''}`}
           />
       ))}
+          <div className="caption-banner">
+              <h2><strong>{maps[index].year}</strong></h2>
+          </div>
       </div>
   );
 }
