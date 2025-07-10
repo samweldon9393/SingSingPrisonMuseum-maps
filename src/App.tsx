e6279fdc5e75673c218b2cb584a32667fef763d3
Sam <samweldon9393@gmail.com>
2025-07-10 16:37:56 -0400

    Add sources routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // src/App.tsx
 import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
 import MainApp from "./MainApp";
+import LandingPage from "./LandingPage";
+import Sources from "./Sources";
 
 function App() {
   const [language, setLanguage] = useState<'en' | 'es'>('en');
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/map" element={<MainApp />} />
+      <Route path="/sources" element={<Sources />} />
     </Routes>
   );
 }
