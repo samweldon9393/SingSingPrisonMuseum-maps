10a33e5a4cbaae08686e4cf408bf189fe5ccdd52
Sam <samweldon9393@gmail.com>
2025-08-17 12:58:34 -0400

    Minor formatting

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -17,21 +17,21 @@ const Popup: React.FC<Props> = ({ regionId, onClose, captionMap }) => {
   const imageUrl = caption ? `${process.env.PUBLIC_URL}${caption[0]}` : null;
 
   return (
-      <DraggableWrapper defaultPosition={{ x: 100, y: 100 }}>
-    <div className="popup">
-    <div className="popup-header">
-      <button className="close-button" onClick={onClose}>×</button>
-      <h3>{regionId}</h3>
-      </div>
-      {imageUrl ? (
-        <>
-          <img src={imageUrl} alt={regionId} className="popup-image" />
-          <i>{caption[1]}</i>
-        </>
-      ) : (
-        <p>No image available.</p>
-      )}
-    </div>
+    <DraggableWrapper defaultPosition={{ x: 100, y: 100 }}>
+        <div className="popup">
+          <div className="popup-header">
+            <button className="close-button" onClick={onClose}>×</button>
+            <h3>{regionId}</h3>
+          </div>
+          {imageUrl ? (
+            <>
+              <img src={imageUrl} alt={regionId} className="popup-image" />
+              <i>{caption[1]}</i>
+            </>
+          ) : (
+            <p>No image available.</p>
+          )}
+        </div>
     </DraggableWrapper>
   );
 };
